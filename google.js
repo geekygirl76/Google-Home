eab36a2ca55c1e97d8c50147d6008f7742815255
Ling Rosenstein <greekyogurtgirl@gmail.com>
2015-01-07 08:26:19 -0500

    add modals and other features

--- google.js
+++ google.js
@@ -14,12 +14,46 @@ $(function(){
                 $(".bell-frame").toggleClass("no-display");
             }
         } ;
+        if (!($(event.target).closest(".plus-li").hasClass("plus-li"))){
+
+            if(!($(".plus-frame").hasClass("no-display"))){
+                $(".plus-frame").toggleClass("no-display");
+            }
+        } else{
+            event.stopPropagation();
+            return;
+            // console.log($(".plus-li").hasClass("no-display"));
+//             console.log("keep the same");
+        };
+    });
+    $("#close-icon").on("click",function(event){
+
+        event.stopPropagation();
+        event.preventDefault();
+        $("#public").css({"display":"none"});
+        $("#add-input").attr("placeholder","To: Add names, circles, or email addresses");
     });
     $(".grid-li").on("click",function(event){
 
         event.preventDefault();
         $(this).find(".more-ul").addClass("no-display");
         $(this).find(".frame").toggleClass("no-display");
+    });
+    $(".frame").on("click",function(event){
+        event.stopPropagation();
+
+    });
+    $(".bell-frame").on("click",function(event){
+        event.stopPropagation();
+
+    });
+    $(".plus-frame").on("click",function(event){
+        event.stopPropagation();
+
+    });
+    $(".plus-li").on("click",function(event){
+        event.preventDefault();
+        $(".plus-frame").toggleClass("no-display");
     });
     $("li.more").on("click", function(event){
         event.stopPropagation();
@@ -33,4 +67,22 @@ $(function(){
         event.preventDefault();
         $(this).find("div.bell-frame").toggleClass("no-display");
     });
+    $("#user-face").on("click", function(event){
+        event.stopPropagation();
+        event.preventDefault();
+        $(".modal").removeClass("no-display");
+        $("header").css({"color":"#ccc","background":"#bbb"});
+        $("main").css({"color":"#ccc"});
+        $("footer").css({"color":"#ccc","background":"#bbb"});
+        $(".plus-frame").css({"color":"#ccc","background":"#bbb"});
+
+    });
+    $("#search-close").on("click",function(event){
+        $(".modal").addClass("no-display");
+        $("header").css({"color":"black","background":"white"});
+        $("main").css({"color":"black"});
+        $("footer").css({"color":"black","background":"#eee"});
+        $(".plus-frame").css({"color":"black","background":"#eee"});
+    });
+    
 });
